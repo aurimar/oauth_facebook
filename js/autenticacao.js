4f18cb56eea6ca711645e0239f047b50b136be9d
Aurimar <aurimardemiranda@gmail.com>
2016-06-04 11:28:29 -0300

    Autenticação Instagram e Twitter
    Funcionando oauth para as redes twitter, facebook e instagram

--- js/autenticacao.js
+++ js/autenticacao.js
@@ -1,10 +1,50 @@
 ﻿$(document).ready(function(){
-   $('.autenticacao').click(function(){
+   $('.autentica_facebook').click(function(){
 
    		//chave de autenticaçao
       	OAuth.initialize('HbsTzFVYovNPJtDpcUYLs_ko-dY');
 		
 		OAuth.popup('facebook').done(function (result) {           
+		                         
+		    result.me().done(function (data) {               
+		        
+		        accessToken: result.access_token;
+		        
+		        $('.id').html(data.id);
+		        $('.name').html(data.name);
+
+		    })
+
+		});
+
+   }); 
+
+    $('.autentica_twitter').click(function(){
+
+   		//chave de autenticaçao
+      	OAuth.initialize('HbsTzFVYovNPJtDpcUYLs_ko-dY');
+		
+		OAuth.popup('twitter').done(function (result) {           
+		                         
+		    result.me().done(function (data) {               
+		        
+		        accessToken: result.access_token;
+		        
+		        $('.id').html(data.id);
+		        $('.name').html(data.name);
+
+		    })
+
+		});
+
+   }); 
+
+     $('.autentica_instagram').click(function(){
+
+   		//chave de autenticaçao
+      	OAuth.initialize('HbsTzFVYovNPJtDpcUYLs_ko-dY');
+		
+		OAuth.popup('instagram').done(function (result) {           
 		                         
 		    result.me().done(function (data) {               
 		        
