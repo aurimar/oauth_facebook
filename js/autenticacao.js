cae5067fa885cfeff27adf47f9c412ae023d7114
Aurimar <aurimardemiranda@gmail.com>
2016-06-04 10:52:41 -0300

    Pagina de autenticação html
    Funcionando e trazendo id e name

---
+++ js/autenticacao.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function(){
+   $('.autenticacao').click(function(){
+
+   		//chave de autenticaçao
+      	OAuth.initialize('HbsTzFVYovNPJtDpcUYLs_ko-dY');
+		
+		OAuth.popup('facebook').done(function (result) {           
+		                         
+		    result.me().done(function (data) {               
+		        
+		        accessToken: result.access_token;
+		        
+		        $('.id').html(data.id);
+		        $('.name').html(data.name);
+
+		    })
+
+		});
+
+   }); 
+});
